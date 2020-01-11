08c956c1f77fec78988333760b846b2ef7ded0f0
annie <anniey9@hotmail.com>
2020-01-11 17:12:32 -0500

    weather project

---
+++ src/components/form.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+
+export default class form extends Component {
+
+
+    render(){
+        return(
+            <div className="form" >
+            <div className="output"> City: {this.props.city}</div>
+            <div className="output"> {this.props.date1} celsius: {this.props.celsiusDay1}</div>
+            <div className="output"> {this.props.date2} celsius: {this.props.celsiusDay2}</div>
+            <div className="output"> {this.props.date3} celsius: {this.props.celsiusDay3}</div>
+            <div className="output"> {this.props.date4} celsius: {this.props.celsiusDay4}</div>
+            <div className="output"> {this.props.date5} celsius: {this.props.celsiusDay5}</div>
+            <div className="output"> {this.props.error}</div>
+            </div>
+                
+        )
+    }
+}
---
+++ src/components/weather.js
@@ -0,0 +1,137 @@
+import React,{Component} from 'react';
+import './weather.css';
+import Form from './form.js';
+
+const APIKEY ="8f95342839eba25e1121275e142e4225";
+
+export default class weather extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state={
+            loading: false,
+            city: undefined,
+            list:[],
+            findtemp: [],
+            celsius: undefined,
+            date: [],
+            // temperature: undefined,
+            error: undefined
+
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    async getWeather (city){
+        console.log(city);
+        const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKEY}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        this.setState({
+            city: data.city.name,
+            loading: true,
+            list: data.list,
+            celsius: this.findtemp(data.list),
+            date: this.findDate(data.list),
+            weather: this.findWeather(data.list),
+            error: " "
+            })
+            console.log("list:",this.state.list);
+            console.log("celsius:",this.state.celsius);
+
+    }
+    findWeather=(list) =>{
+        console.log("weather:",list[0]['weather'][0]['main']);
+
+        var newWeather = new Array(5);
+        for(var j =0; j<5; j++){
+            newWeather[j] = 0;
+        }
+        for(var i=0; i< list.length; i++){
+            newWeather[i] = list[i]['weather'][0]['main'];
+        }
+        console.log(newWeather);
+    }
+
+    findDate=(list) =>{
+        // console.log('list', this.state.list);
+        
+        var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const dates = list.map((item, i) => {
+             return item.dt_txt.split(" ")[0];
+        }).filter((item, i, arr) => { return arr.indexOf(item) === i; });
+
+        console.log('dates', dates);
+        var day = new Array(dates.length);
+        for(var i=0; i<dates.length; i++){
+            day[i] = weekdays[new Date(dates[i]).getDay()];
+
+        }
+        // console.log(day);
+        return day;
+    }
+
+    findtemp=(list)=>{
+        // console.log("temp:",list[0]['main']['temp']);
+        var newlist = new Array(5);
+        for(var j =0; j<5; j++){
+            newlist[j] = 0;
+        }
+        console.log(newlist);
+        for(var i=0; i< list.length; i++){
+            newlist[Math.floor(i / 8)] += list[i]['main']['temp'];
+        }
+        console.log("list:", newlist);
+        for(var a in newlist){
+            newlist[a] /= 8;
+            newlist[a] -= 273.15;
+        }
+        // console.log("a:",  Math.floor(newlist));
+        var b = 0;
+        while(b < 5){
+            newlist[b] = newlist[b].toFixed(2);
+            b++
+        }
+        return newlist;
+    }
+
+        handleSubmit=(e) =>{
+            e.preventDefault();
+            this.getWeather(e.target.city.value);
+
+        }
+        render() {
+        return (
+          <div>
+              <form onSubmit={this.handleSubmit}>
+                <input
+                  type="text"
+                  name="city"
+                  placeholder="Search a City..."
+                  ref="city"
+                />
+                <button>Get Weather</button>
+              </form>
+              { this.state.loading ?
+              <Form
+                city ={this.state.city}
+                celsiusDay1 ={this.state.celsius[0]}
+                celsiusDay2 = {this.state.celsius[1]}
+                celsiusDay3 = {this.state.celsius[2]}
+                celsiusDay4 = {this.state.celsius[3]}
+                celsiusDay5 = {this.state.celsius[4]}
+                date1 = {this.state.date[0]}
+                date2 = {this.state.date[1]}
+                date3 = {this.state.date[2]}
+                date4 = {this.state.date[3]}
+                date5 = {this.state.date[4]}
+              /> : null}
+            {/* <div className="output"> City: {this.state.city}</div> */}
+          </div>
+
+
+        )
+
+      };
+    }
